3f287d5dc830f266c7d71eebb699c5bf4eaebf05
morvarid mahmoudi far <morvaridmahmoudi@gmail.com>
2022-11-21 10:25:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ database.once('connected', () => {
 })
 
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.join(__dirname, "../client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }

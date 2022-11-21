8a83965de1ea016bcd504543699e65b4dfc69b4c
morvarid mahmoudi far <morvaridmahmoudi@gmail.com>
2022-11-21 10:00:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ database.once('connected', () => {
 })
 
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
 
 
 // ---get all the notes
